8e04989ae3d53073e12d9efadd16757525a462bc
manal adam <manaal@gmail.com>
2025-02-11 20:23:48 +0000

    java project to resolve #2

---
+++ src/test/MainTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    MainTest() {
+    }
+
+    @Test
+    void testMain() {
+        Assertions.assertTrue(true);
+    }
+}
