c9c6b67bcb0cc5c2308aa0c676ae8fe2c496170d
manal adam <manaal@gmail.com>
2025-03-25 23:04:17 +0000

    collision simulation #19

---
+++ java-service/src/test/java/com/kalschatzi/HashmapCollisionTest.java
@@ -0,0 +1,31 @@
+package test.java.com.kalschatzi;
+
+import org.junit.jupiter.api.Test;
+import com.kalschatzi.Key;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class HashmapCollisionTest {
+
+
+    @Test
+    void collisionTest(){
+        HashMap<Key, String> map = new HashMap<>();
+
+        // Create keys with the same hash code
+        com.kalschatzi.Key key1 = new com.kalschatzi.Key("key1");
+        com.kalschatzi.Key key2 = new com.kalschatzi.Key("key2");
+
+        // insert values into hashmap
+        map.put(key1, "Value for key1");
+        map.put(key2, "Value for key2");
+
+        // test asserts that both values were added to the hashmap, proving collision was resolved
+        assertEquals(2, map.size());
+
+
+    }
+
+}
