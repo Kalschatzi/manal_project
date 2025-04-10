9f11dad728a59d51542c1479ceb77c5445fd2282
Tiago Alves <tmcalves@gmail.com>
2025-04-10 18:15:30 +0100

    fix test execution

--- springboot-app/src/test/java/com/Kalschatzi/springboot_app/SpringbootAppApplicationTests.java
+++ springboot-app/src/test/java/com/Kalschatzi/springboot_app/SpringbootAppApplicationTests.java
@@ -3,11 +3,14 @@ package com.Kalschatzi.springboot_app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class SpringbootAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail("This is a test failure example");
 	}
 
 }
